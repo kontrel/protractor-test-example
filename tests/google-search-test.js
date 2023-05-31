4750f2c0af3d3550a89f2affeb47a8d2c4a03235
Konrad Trelka <konradtrelek@gmail.com>
2023-05-31 20:38:09 +0200

    Add before and after tests functions

--- tests/google-search-test.js
+++ tests/google-search-test.js
@@ -1,21 +1,29 @@
 describe('google search test', function() {
 
-    it('should search from google', function() {
-      browser.ignoreSynchronization = true;
-      browser.get('https://google.com/');
-      
-      element(by.id('L2AGLb')).click();
-      element(by.name('q')).sendKeys('protractor');
-      
-      element(by.xpath('(//input[@name="btnK"])[2]')).click();
-      let searchResult = element(by.xpath('(//a[@jsname="qOiK6e"]/h3)[1]'))
-  
-
-      searchResult.getText('value').then(function (actual) {
-        expect('Protractor - end-to-end testing for AngularJS').toEqual(actual);
-    });
-
-  
-    });
+  beforeEach(function() {
 
   });
+
+  afterEach(function() {
+  
+  });
+
+  it('should search from google', function() {
+    browser.ignoreSynchronization = true;
+    browser.get('https://google.com/');
+      
+    element(by.id('L2AGLb')).click();
+    element(by.name('q')).sendKeys('protractor');
+      
+    element(by.xpath('(//input[@name="btnK"])[2]')).click();
+    let searchResult = element(by.xpath('(//a[@jsname="qOiK6e"]/h3)[1]'));
+  
+
+    searchResult.getText('value').then(function (actual) {
+      expect('Protractor - end-to-end testing for AngularJS').toEqual(actual);
+    });
+
+  
+  });
+
+});
