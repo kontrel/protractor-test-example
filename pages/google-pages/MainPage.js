ca9bce6cf120577c3275c6554cfe8e3a5c9db9ba
Konrad Trelka <konradtrelek@gmail.com>
2023-05-31 20:34:57 +0200

    Page Object Model

---
+++ pages/google-pages/MainPage.js
@@ -0,0 +1,22 @@
+class MainPage {
+
+    constructor() {
+
+        let searchInput = element(by.name('q'));
+        let searchButton = element(by.xpath('(//input[@name="btnK"])[2]'));
+        let acceptConsents = element(by.id('L2AGLb'));
+
+        this.acceptConsents = async function () {
+            if(await acceptConsents.isPresent()) {
+                acceptConsents.click();
+            }
+        };
+
+        this.search = function (searchValue) {
+            searchInput.sendKeys(searchValue);
+            searchButton.click();
+        };
+
+    }
+}
+module.exports = new MainPage();
