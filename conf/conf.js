95d893e09b6560f50ce3b005cd2a37fcb64f485d
Konrad Trelka <konradtrelek@gmail.com>
2023-06-01 21:45:15 +0200

    First parabank test

--- conf/conf.js
+++ conf/conf.js
@@ -2,19 +2,23 @@ exports.config = {
 
     multiCapabilities: [ 
       {
-        'browserName': 'chrome'
+        'browserName': 'chrome',
+        chromeOptions: {
+          args: [
+                   'start-maximized'
+                ]
+        }
       },
     ],
 
     seleniumAddress: 'http://localhost:4444/wd/hub',
-
-    SELENIUM_PROMISE_MANAGER: false,
 
     maxSessions: 1,
     
     specs: [
       // '../tests/test1.js',
       // '../tests/test2.js',
-      '../tests/google-search-test.js'
+      // '../tests/google-search-test.js',
+      '../tests/parabank-test.js'
     ]
   };
